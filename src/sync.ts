2a94a2d644ac4f68b35ce51f65386618ede9920a
Bilal0335 <bilalhussain42201.com>
2024-07-25 01:25:45 +0500

    update

--- src/sync.ts
+++ src/sync.ts
@@ -1,36 +1,36 @@
-// console.log('Start')
+console.log('Start')
 
-// let userName = 'Bilal Hussain'
-// console.log(userName)
+let userName = 'Bilal Hussain'
+console.log(userName)
 
-// let age: number = 18
-// if (age < 18) {
-//   console.log('You are not adult')
-// } else {
-//   console.log('You are adult')
-// }
+let age: number = 18
+if (age < 18) {
+  console.log('You are not adult')
+} else {
+  console.log('You are adult')
+}
 
-// const bestFriend: string[] = ['Hamza', 'Junaid', 'David', 'John']
-// for (let i = 0; i < bestFriend.length; i++) {
-//   console.log(`${bestFriend[i]} is my best bestFriend.`)
-// }
+const bestFriend: string[] = ['Hamza', 'Junaid', 'David', 'John']
+for (let i = 0; i < bestFriend.length; i++) {
+  console.log(`${bestFriend[i]} is my best bestFriend.`)
+}
 
-// type TCar = {
-//   model: string
-//   year: number
-//   isUsed: boolean
-//   brand: string
-// }
-// let car: TCar = {
-//   brand: 'Toyota',
-//   model: 'Carolla',
-//   isUsed: false,
-//   year: 2020
-// }
+type TCar = {
+  model: string
+  year: number
+  isUsed: boolean
+  brand: string
+}
+let car: TCar = {
+  brand: 'Toyota',
+  model: 'Carolla',
+  isUsed: false,
+  year: 2020
+}
 
 
-// console.log(car.brand);
-// console.log(car['model']);
+console.log(car.brand);
+console.log(car['model']);
 
-// console.log('The End');
+console.log('The End');
 
